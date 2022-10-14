cbea756c8f5586b448acbf53bf4753c9a82a00f2
rezaulislam <mrezaulislam.t@gmail.com>
2022-10-14 08:37:01 +0600

    Localization tool done

--- src/MainClass.java
+++ src/MainClass.java
@@ -6,9 +6,11 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class MainClass {
+	
 	static String commonPublic = "public static final String ";
 	static String commonPrivate = "private var ";
     static String label = "LABEL";
+    static String dotLine = "--------------------------------------------------------------------------------------------";
     
     static Path inputFilePath = Path.of("src/input.txt");
     static Path configPath =  Path.of("src/config.txt");
@@ -17,46 +19,67 @@ public class MainClass {
     
 	public static void main(String[] args) throws Exception{
 		 
-        
-        
         String config = Files.readString(configPath);
         String contents = Files.readString(inputFilePath);
    
         String[] contentsAry = contents.split(config);
         StringBuffer constraintVariable = generateAllConstraint(contentsAry);
-        generateMemberVariable(contentsAry);
+        StringBuffer memberVariable = generateMemberVariable(contentsAry);
+        StringBuffer assignVariable = generateAssignedMemberVariable(contentsAry);
+        StringBuffer keys = generateAllKeys(contentsAry,config);
         
-        
+        constraintVariable.append("\n\n"+dotLine+"\n\n"+memberVariable+"\n\n"+dotLine+"\n\n").append(assignVariable+"\n\n"+dotLine+"\n\n").append(keys);
+        Files.writeString(outputFilePath, constraintVariable);
+        System.out.println("Your Output file is ready to use.");
   
 	}
 	
 	public static StringBuffer generateAllConstraint(String []contentsAry ) throws IOException {
-		//public static final String LABEL_DOCTOR_PROFILE = "label_doctor_profile";
 		StringBuffer sb = new StringBuffer();
+		sb.append(""+dotLine+"\n\n");
 		for(String s : contentsAry) {
         	s = s.trim();
         	String CONSTRAINT = commonPublic+""+label+"_"+s.toUpperCase().replaceAll(" ", "_")+" = \"" + label.toLowerCase()+"_"+s.toLowerCase().replaceAll(" ", "_")+"\";";
-        	System.out.println(""+CONSTRAINT);
         	sb.append(CONSTRAINT+"\n");
         }
 		return sb;
 	}
 
 	public static StringBuffer generateMemberVariable(String []contentsAry ) throws IOException {
-		//private var labelDoctorsConsultationTimeIs: String? = ""
 		StringBuffer sb = new StringBuffer();
 		for(String s : contentsAry) {
         	s = s.trim().toLowerCase();
         	s = toCamelCase(s);
-        	
         	String memberVariable = commonPrivate+""+label.toLowerCase()+s+":String? = \"\";";
-        	System.out.println(""+memberVariable+" ");
+        	sb.append(memberVariable+"\n");	
+        }
+		return sb;
+	}
+	
+	public static StringBuffer generateAssignedMemberVariable(String []contentsAry ) throws IOException {
+		StringBuffer sb = new StringBuffer();
+		for(String s : contentsAry) {
+        	s = s.trim().toLowerCase();
+        	String cs = s;
+        	s = toCamelCase(s);
         	
+        	String memberVariable = label.toLowerCase()+s+" = getSingleLocale(LocalisationConstants."+ label+"_"+cs.toUpperCase().replaceAll(" ", "_")+");";
+        	sb.append(memberVariable+"\n");
         }
 		
 		return sb;
 	}
 	
+	public static StringBuffer generateAllKeys(String []contentsAry , String config) throws IOException {
+		StringBuffer sb = new StringBuffer();
+		for(String s : contentsAry) {
+        	s = s.trim();
+        	String Keys =  label.toLowerCase()+"_"+s.toLowerCase().replaceAll(" ", "_")+config;
+        	sb.append(Keys+"\n");
+        }
+		return sb;
+	}
+
 	static String toCamelCase(String s){
 		   String[] parts = s.split(" ");
 		   String camelCaseString = "";
